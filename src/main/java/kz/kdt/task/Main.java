98ab8502a640949f63a8fb3e37efdbf67e26434e
s.kasaev <kasaev.serzhan@yandex.ru>
2021-02-28 21:19:12 +0600

    FIX

--- src/main/java/kz/kdt/task/Main.java
+++ src/main/java/kz/kdt/task/Main.java
@@ -12,10 +12,12 @@ public class Main {
     public static void main(String[] args) {
         DeveloperDao developerDao = new DeveloperDao();
         DesignerDao designerDao = new DesignerDao();
+        //менять id у каждого и удалять от сюда после добавление в БД. тут id=4L
         Employee developer = new Developer(4L,"Yerzhan", "Yerzhayev", "Turan 20", 50000, "HB", DeveloperType.FRONTEND);
         developerDao.save((Developer) developer);
         //developerDao.findAll();
 
+        //менять id у каждого и удалять от сюда после добавление в БД, тут id=1L
         Employee designer = new Designer(1L,"Nurzhan", "Nurzhayev", "Turan 20", 50000, "HB", DesignerType.UIUX);
         designerDao.save((Designer) designer);
 
